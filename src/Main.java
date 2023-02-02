e7660f6f0444fca036294e4dbc5ab08890a99ca5
bomblock <ekwotmd@gmail.com>
2023-02-03 00:27:15 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     // todo : if 문의 3문장을 하나의 문장으로 해석하는 삼항연산
+        boolean isMarried = true;
+
+        String str;
+
+        str = isMarried ? "결혼했다." : "결혼 안 했다.";
+
+       // if (isMarried)
+    }
+}
